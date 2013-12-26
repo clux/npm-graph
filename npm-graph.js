5a17d0ada01559524f363fce607caaddea338e15
clux <analsandblaster@gmail.com>
2013-12-26 12:35:49 +0000

    no need to try-catch wrap anymore, all errors that we expect are stream errors from failed resolves

--- npm-graph.js
+++ npm-graph.js
@@ -15,40 +15,37 @@ var isNotBuiltin = function (id) {
 module.exports = function (file, name, cb, opts) {
   var allDeps = {};
   opts = opts || {};
-  try {
-    mdeps(file, { filter: isNotBuiltin }).on('data', function (o) {
-      allDeps[o.id] = o.deps;
-    }).on('error', function (err) {
-      // hopefully we can get these in the future without 'end' not happening
-      console.warn(err.message);
-    }).on('end', function () {
-      //console.log(JSON.stringify(allDeps, null, '\t'));
 
-      // build a dependency tree from the flat mdeps list by recursing
-      var topTree = { name: name };
-      var traverse = function (depObj, loc) {
-        loc.deps = loc.deps || {};
-        Object.keys(depObj).sort().forEach(function (key) {
-          if (!opts.showLocal && ['\\', '/', '.'].indexOf(key[0]) >= 0) {
-            // keep local deps private, but keep inspecting them for modules
-            traverse(allDeps[depObj[key]] || {}, loc);
-          }
-          else {
-            // put new modules under new headers under current location
-            loc.deps[key] = { name: key };
-            traverse(allDeps[depObj[key]] || {}, loc.deps[key]);
-          }
-        });
-      };
-      traverse(allDeps[file], topTree);
+  mdeps(file, { filter: isNotBuiltin }).on('data', function (o) {
+    allDeps[o.id] = o.deps;
+  }).on('error', function (err) {
+    // hopefully we can get these in the future without 'end' not happening
+    console.warn(err.message);
+  }).on('end', function () {
+    //console.log(JSON.stringify(allDeps, null, '\t'));
 
-      var filterFn = function (o) {
-        return opts.showBuiltins || isNotBuiltin(o.name);
-      };
-      cb(null, topiary(topTree, 'deps', shapeFn, filterFn));
-    });
-  }
-  catch (e) {
-    cb(e);
-  }
+    // build a dependency tree from the flat mdeps list by recursing
+    var topTree = { name: name };
+    var traverse = function (depObj, loc) {
+      loc.deps = loc.deps || {};
+      Object.keys(depObj).sort().forEach(function (key) {
+        if (!opts.showLocal && ['\\', '/', '.'].indexOf(key[0]) >= 0) {
+          // keep local deps private, but keep inspecting them for modules
+          traverse(allDeps[depObj[key]] || {}, loc);
+        }
+        else {
+          // put new modules under new headers under current location
+          loc.deps[key] = { name: key };
+          traverse(allDeps[depObj[key]] || {}, loc.deps[key]);
+        }
+      });
+    };
+    traverse(allDeps[file], topTree);
+
+    var filterFn = function (o) {
+      return opts.showBuiltins || isNotBuiltin(o.name);
+    };
+    cb(null, topiary(topTree, 'deps', shapeFn, filterFn));
+  });
+
 };
