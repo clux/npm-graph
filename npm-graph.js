4418ffbe7a723b3785ae17c428a9146c6c1da9bb
clux <analsandblaster@gmail.com>
2014-07-11 19:22:12 +0100

    mostly fix up the module

--- npm-graph.js
+++ npm-graph.js
@@ -11,16 +11,22 @@ var coreModules = ['assert', 'buffer', 'child_process', 'cluster',
 var isNotBuiltin = function (id) {
   return coreModules.indexOf(id) < 0;
 };
+var isNotCoverage = function (id) {
+  return !(/\-cov\//).test(id);
+};
+var isScannedForDeps = function (id) {
+  return isNotBuiltin(id) && isNotCoverage(id);
+};
 
 module.exports = function (file, name, cb, opts) {
   var lookup = {};
   opts = opts || {};
 
   var showInTree = function (o) {
-    return opts.showBuiltins || isNotBuiltin(o.name);
+    return (opts.showBuiltins || isNotBuiltin(o.name)) && isNotCoverage(o.name);
   };
 
-  mdeps(file, { filter: isNotBuiltin }).on('data', function (o) {
+  mdeps(file, { filter: isScannedForDeps }).on('data', function (o) {
     // o is object with `id` and `deps` = { relReq : id }
     lookup[o.id] = Object.keys(o.deps).map(function (key) {
       return { name: key, path: o.deps[key] };
@@ -30,21 +36,23 @@ module.exports = function (file, name, cb, opts) {
     console.warn(err.message);
   }).on('end', function () {
     // build a dependency tree from the flat mdeps list by recursing
+    var covered = []; // only cover unique paths once to avoid stack overflow
     var traverse = function (currDeps, loc) {
       loc.deps = loc.deps || [];
-      console.log('curr', currDeps, '&&&&&&&&&&&&&&&&')
       currDeps.sort().forEach(function (obj) {
-        var key = obj.name
-          , children = lookup[obj.path]
-          , isRecorded = (opts.showLocal || ['\\', '/', '.'].indexOf(key[0]) < 0);
+        if (covered.indexOf(obj.path) < 0) {
+          var key = obj.name
+            , isRecorded = (opts.showLocal || ['\\', '/', '.'].indexOf(key[0]) < 0)
+            , res = isRecorded ? { name: key } : loc;
 
-        var res = isRecorded ? { name: key } : loc;
-        if (isRecorded) {
-          // NB: !isRecorded => only inspect the file for recorded deps
-          loc.deps.push(res);
+          if (isRecorded) {
+            // NB: !isRecorded => only inspect the file for recorded deps
+            loc.deps.push(res);
+          }
+          covered.push(obj.path);
+          // recurse (!isRecorded => keep adding to previous location)
+          traverse(lookup[obj.path] || [], res);
         }
-        // recurse (!isRecorded => keep adding to previous location)
-        traverse(lookup[obj.path] || [], res);
       });
     };
     var tree = { name: name };
